f94be826c1dbb652c98e1aa70fc85164cad40e0a
Jahanavip2816 <jahanavi.p@saksoft.com>
2025-10-23 17:58:32 +0530

    Today Tasks

---
+++ LinqPractice/Linq6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+    public class Linq6
+    {
+        static void Main()
+        {
+            string[] Month = { "Jan", "Feb", "Mar", "Apr", "May", "jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
+            Console.Write("Name of the Months: ");
+
+            var m = from Jan in Month
+                       select Month;
+
+            foreach (var months in m)
+             Console.WriteLine(months);
+
+        }
+    }
+
+}
+
