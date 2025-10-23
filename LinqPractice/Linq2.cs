5aaad3b844df83dcff1892a3ece84f0880b09e9c
Jahanavip2816 <jahanavi.p@saksoft.com>
2025-10-23 15:45:19 +0530

    Add project files.

---
+++ LinqPractice/Linq2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqPractice
+{
+   static class Linq2
+    {
+       public  static void Main()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 4 };
+            var res = numbers.Where(n => n > 3).Select(n => n * 2);
+
+            Console.WriteLine("the numbers are");
+            foreach(var VrNum in res)
+            {
+                Console.WriteLine(VrNum);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinqPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿// Three parts of a query operation execute
+
+using LinqPractice;
+using System;
+using System.Linq;
+
+class Linq1
+{
+    public static void Main(string[] args)
+    {
+        int[] n1 = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Console.WriteLine("Basic structure of LINQ");
+
+        var nQuery=
+            from VrNum in n1
+            where (VrNum % 2) == 0
+            select VrNum;
+
+        Console.WriteLine("Numbers that produces 0");
+        foreach(int VrNum in nQuery)
+        {
+            Console.WriteLine("{0}", VrNum);
+        }
+
+
+    }
+}
